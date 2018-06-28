bd6297754d53cc63c89e367cf8fe3f9836c53d95
Dre West <40345012+duinotech@users.noreply.github.com>
2018-06-28 18:44:09 +1000

    uploaded to proper repo

--- README.md
+++ README.md
@@ -1,1 +1,124 @@
-# IoT Wireless Sign
+# WiFi Sign
+
+_Author's Note: This project immediately went into revisions after publications, as the mini protoboard only just barely fits everything in, and we felt it would be easier to use a bigger protoboard to make. Thus, some pictures are from different versions, but the connections are all following the same circuit diagram. If you have any issues or need assistance building this project, you can email me directly at DWest@jaycar.com.au_
+
+This neat project can instantly decorate your shop window, bedroom door, or loungeroom. It allows you to mount a dot matrix display somewhere in your house and connect to it through the same network. Then using a custom HTTP server, you can access the sign through its network address and change it to display a clock, text, or anything else that you might want.
+
+
+
+## Bill of Materials
+| Qty | Code | Description |
+| --- | --- | ---|
+|1 | [XC4622](jaycar.com.au/p/XC4622) | White dot matrix display panel
+|1 | [XC3802](jaycar.com.au/p/XC3802) | ESP8266
+|1 | [ZS5807](jaycar.com.au/p/ZS5807) | 7407 hex buffer IC
+|1 | [HP9550](jaycar.com.au/p/HP9550) | universal pre-punched experimenters board
+|1 | [RR0596](jaycar.com.au/p/RR0596) | 10k resistor pack
+|1 | [RR0572](jaycar.com.au/p/RR0572) | 1k resistor pack
+
+## Connections
+
+![](images/schematic.png)
+This is the general circuit diagram for the set up. We connect the ESP8266 to the DMD through a 7407 hex buffer, which acts as a makeshift “level converter” to change from the 3.3v ESP to 5v for the panel. The ESP comes with multiple headers, so we will use both the male ( to mount the ESP) and the female (to connect to DMD) headers.
+
+It's simple as that really.
+
+![](images/hex.png)
+
+This is the hex buffer chip that we're using. Data pins must go through one of the A pins, then the corresponding Y pin then goes to the DMD. This is a cheap way to make a makeshift logic level converter. These are open-collector outputs, so we must use pull up resistors so that they work correctly.
+
+![](images/DMD.png)
+
+The DMD pinout is much like what you’ve seen before, with the 3 serial lines in the middle and the A/B/OE pins to the top. The pins (3,5,7,9,11,13,15) in this diagram are all connected to ground so any one of these would do for a ground connection.
+
+
+
+The final point to note is that the ESP requires two pull-ups to correctly boot into itself. If the pins are floating, it might want to go into “programming mode” and not run its own commands. These are simple 10k resistors connecting D8-GND and D3-3v3. These are shown in the schematic diagram as R7 and R8.
+
+Read further and look at some pictures to get an understanding of how the board is laid out and fits to the panel.
+
+## Downloads
+* [Source code](downloads/WifiSign.zip)
+
+## Software libraries
+
+|Library | Author |
+|---| ---|
+|ESP8266WiFi | Ivan Grokhotkov |
+
+
+ * be sure to change your arduino environment to program the ESP8266 as shown in the [manual](https://www.jaycar.com.au/medias/sys_master/images/9110200188958/XC3802-manualMain.pdf)
+
+## Assembly
+
+As for the layout of the board, Mine is shown. It’s not quite the prettiest board but it works for the time being.
+
+lay out the board however you want, following the connection diagram above.
+
+![](images/board.jpg)
+
+The final note is using the 5v wall socket supply to power the device. you can bridge wires between the DMD's VCC/GND screw terminals to 5V and GND on your protoboard to make the whole Assembly run off the wall socket.
+
+As mentioned, this is a revisited version, the original version was designed to fit on the protoshield like such:
+
+![](images/IMAG0350.jpg)
+
+however with such a tight space it was hard to make things nice and connected. many many resistor leads pushed down the same PCB hole than the IC, simply because there wasn't any spare PCB holes to use.
+
+![](images/IMAG0353.jpg)
+
+This is the second attempt at the first version, and by this point it was only half-done and had no more room to spare.
+
+While it looks clean and tidy, it's like a jaguar car. A real mess underneath and a pain to fix problems with.
+
+So we went ahead with version two, post publication. Same circuit, same everything, but laid out on a bigger board with more room.
+
+it also uses an on-board connection for the DMD panel, to make things nice and tidy.
+
+![](images/IMAG0348.jpg)
+_disconnected from panel_
+
+![](images/IMAG0349.jpg)
+_connected to panel_
+
+note the USB for the ESP pointing out the bottom.
+
+Follow the circuit diagram if you get stuck. Each pin from the DMD connector must be tied to 5V using resistors, and they connect to the output pins on the hex chip.
+
+each input of the hex chip is coming straight from the ESP.
+then the ESP has 2 resistors to boot properly.
+
+
+## Programming
+
+* Remember! [ESP8266 on Arduino ](https://www.jaycar.com.au/medias/sys_master/images/9110200188958/XC3802-manualMain.pdf)
+
+
+Download the source code and extract into a new folder, open the .ino file in the Arduino IDE.
+
+You should see three variables there is to set:
+
+```c++
+const char* ssid = "";  //your wifi network name
+const char* pwd = "";   //your wifi password
+String location = "mainroom";  //set this to the location where the sign is, one word.
+```
+
+set these to your wifi name, password, and the location of the sign, then upload to the ESP.
+
+## Use
+
+Once the ESP is connected to the WiFi network, it makes a website hosted on "http://mainroom.local" which "mainroom" is the room name you set before.
+
+if that does not work, you can use the Serial Monitor to see what the IP is when it boots.
+
+when you are at the website, you can find a radio button for "text" or "clock" and a text field. set the text to what you want and hit submit.
+
+you should see the sign showing up with the text you've set.
+
+## Future Improvements
+* At the moment there is text and a clock, you can specify your own command by making a new
+"param switch" in the code. See if you can send binary data to turn on/off each separate pixel using dmd.setPixel(x,y,v);
+* The DMD also has a brightness setting, use this in the control panel.
+* Get a small picture frame to frame the sign.
+* You can interface with another arduino and use a small speaker [AS3006](jaycar.com.au/p/AS3006) to play some melodies from your sign as well.
---
+++ WifiSign/WifiSign.ino
@@ -0,0 +1,360 @@
+/*
+ * WiFi sign - Jaycar Electronics
+ * Author Dre West
+ */
+
+// Load Wi-Fi library
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <SPI.h>
+#include <DMD2.h>
+
+
+const char* ssid = "";  //your wifi network name
+const char* pwd = "";   //your wifi password
+String location = "mainroom";  //set this to the location where the sign is, one word.
+
+
+
+const int SIGN_WIDTH = 1; //these make the sign 1x1 DMD panel
+const int SIGN_HEIGHT = 1;
+
+WiFiServer server(80);  //start a WiFi server on port 80
+HTTPClient http;
+String command;         //this is the string that we are going to get from the server.
+String line;            //this is a simple line buffer to process the entire text from the server, line by line.
+
+
+void sendPage(WiFiClient c); //this is a function that we can write later, define it now so we can use it while it's empty
+short nextEndpoint(short index, int* endpoint_list, short len); //find the next endpoint from an index point
+int fillEndpointList(char* cmd_str, int len, int* endpoint_list); //fill the endpoint list
+
+bool clockMode = false;
+void setup()
+{
+  Serial.begin(115200);
+
+  //WiFi setup code
+  Serial.print("Connecting to: ");
+  Serial.println(ssid);
+  String hostn = ".local";
+
+  hostn = location.concat(hostn); //put location.local
+  
+  WiFi.hostname(hostn.c_str());   //on latest windows, linux and mac, you can type "<location>.local" to connect to this device instead of pin numbers
+
+  //connect to the network
+  WiFi.begin(ssid,pwd);
+  while(WiFi.status() != WL_CONNECTED){
+    //not connected yet, give it time.
+    delay(500);
+    Serial.print(".");
+  }
+
+  //print out some nice information
+  Serial.println("Wifi Connected");
+  Serial.print("IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("Or try using http://");
+  Serial.println(hostname);
+  server.begin();
+}
+
+void loop()
+{
+  //check for a client
+  WiFiClient client = server.available();
+
+  //clear the command
+  command = "";
+
+  //the next few lines are tricky to understand but forms the basis of network connection programming
+
+  //if we have a client
+  if(client)
+  {
+    //clear the line buffer
+    line = "";
+
+    //while the client is connected
+    while(client.connected())
+    {
+      //if the client has incoming data available
+      if (client.available())
+      {
+
+        //read a byte of that data
+        byte c = client.read();
+
+        //write it to Serial, you should see a letter, because the byte is valid ascii
+        Serial.write(c);
+
+        //if it is a newline character, we have finished this line so we can check it.
+        if(c == '\n')
+        {
+          //begin processing line:
+
+
+          //check if the line starts with "GET" which is a HTML word
+          if(line.startsWith("GET /?"))
+          { 
+            //this line has the params, so copy it into our command buffer
+            command = line;
+
+            // keep getting the information though, as we need to listen to the client before we can respond.
+          }
+
+
+          //check if this line is empty, if it is, we have 2 newlines in a row 
+          //(first one finished last line, this is empty)
+          if (line.length() == 0)
+          { 
+
+            //this is the end of the request. they have sent two new lines in a row, 
+            //so here we respond by sending a page back.
+            Serial.println("end of client request, responding");
+
+            //send a page to the client in response
+            sendPage(client);
+
+            //break out of "while (client connected)"
+            break; 
+
+          } 
+
+          //the line is finished, but it's not GET and it's not empty, 
+          //so we don't care about it. clear out the line buffer
+          line = "";          
+        } 
+        else 
+        {
+          //it is not a newline character, but we stil have a character 
+          //if we got anything other than an \r (we don't want \r)
+          if ( c != '\r')
+          {
+            //add it to the line buffer     
+            line += c;            
+          }
+        }
+        //keep looping through the data sent from the client, 
+      }
+    }
+
+    /*
+     * Here we have finished receiving data so we respond.
+     */
+    //here he's no longer sending or receiving data from us, so check if we have a command and process it.
+    int endpoints[16] = {0}; //have a list of parameter endpoints.
+
+    if(command.length() > 0){
+
+
+      /*
+       * The command line would look something like /?option=text&var=value&var2=value2& ...
+       * so we make a list of 16 endpoints, which stores the index of all the '?' and '&'
+       * then when we find the index of, say, 'option' we can then find the next end point to extract the entire value
+       */
+
+      n_endpoints = fillEndpointList(command.c_str(), command.length(), endpoints);
+
+      //here is where we define our commands.
+
+
+
+      // ?option=""
+      short opt_index_start = command.indexOf("option=")+7; //the start of the command value.
+      short opt_index_end = nextEndpoint(opt_index_start,endpoints,n_endpoints); //end of command value (start of another command)
+
+      String opt_value = command.substring(opt_index_start,opt_index_end);
+
+      Serial.println(command);
+      Serial.print("opt_indexes: ");
+      Serial.print(opt_index_start,DEC);
+      Serial.print("-");
+      Serial.println(opt_index_start,DEC);
+      Serial.print("option=");
+      Serial.print(opt_value);
+      Serial.println();
+
+      //we have the opt_value stored in a string, so we can compare it to see what the user wants to do
+      if(opt_value == "text")
+      {
+        clockMode = false;
+        //we got a text command
+
+
+        // ?text=""
+        short txt_index_start = command.indexOf("text=")+5; //the start of the text value
+        short txt_index_end = nextEndpoint(txt_index_start,endpoints,n_endpoints);
+        String txt_value = command.substring(txt_index,nextEndpoint(txt_index,endpoints,10));
+
+        Serial.println(command);
+        Serial.print("txt_indexes: ");
+        Serial.print(txt_index_start,DEC);
+        Serial.print("-");
+        Serial.println(txt_index_start,DEC);
+        Serial.print("text=");
+        Serial.print(txt_value);
+        Serial.println();
+        // we have text stored in a string, so format this to make it a little more nicer to read
+
+        txt_value.replace("+"," "); //html will automatically change spaces to + signs so we change it back
+
+        Serial.print("setting message to: ");
+        Serial.println(txt_value);
+
+        dmd.drawString(0, 0, F(txt_value.c_str()));
+
+      }
+      else if (opt_value == "clock")
+      { 
+        //we got a clock command
+        clockMode = true;
+      }
+      else
+      {
+        //not sure what this option is is
+        Serial.println("not sure what option is:");
+        Serial.println(opt_value);
+      }
+    }
+    client.stop();                    //close connection
+  }
+
+  if (clockMode)
+  {
+    String datetime;
+    unsigned long current_runtime = millis();
+    if (current_runtime - last_runtime > refresh_runtime)
+    {
+      //get google, they have GMT in the response
+      http.begin("www.google.com");
+      const char* important_headers = { "Date" };
+      http.collectHeaders(important_headers, 1);      
+
+      int rc = http.GET();
+      if(rc > 0)
+      {
+        if(http.hasHeader("Date")){
+          datetime = http.header("Date");
+/*
+        __                              ____                        ________  _________
+  _____/ /_  ____ _____  ____ ____     / __/________  ____ ___     / ____/  |/  /_  __/
+ / ___/ __ \/ __ `/ __ \/ __ `/ _ \   / /_/ ___/ __ \/ __ `__ \   / / __/ /|_/ / / /   
+/ /__/ / / / /_/ / / / / /_/ /  __/  / __/ /  / /_/ / / / / / /  / /_/ / /  / / / /    
+\___/_/ /_/\__,_/_/ /_/\__, /\___/  /_/ /_/   \____/_/ /_/ /_/   \____/_/  /_/ /_/     
+                      /____/                                                           
+*/
+          //here we just write out what we've got as GMT to the panel.
+        //TODO
+        //dmd.drawString(0, 0, F(datetime.c_str()));
+        Serial.println()
+        }
+      }
+      else 
+      {
+        Serial.println("error getting time from google");
+        Serial.println(rc,DEC);
+      }
+    }
+  }
+
+  for(int i = 0; i < 48; i++)
+  {
+    dmd.marqueeScrollX(1);
+    delay(10);
+  }
+  for(int i = 0; i < 48; i++)
+  {
+    dmd.marqueeScrollX(-1);
+    delay(10);
+  }
+}
+//returns the index of the next endpoint, coming from index, for max of len.
+short nextEndpoint(short index, int* endpoint_list, short len){
+  short i;
+  for(i = 0; i < len, i++)
+    if (endpoint_list[i] >= index)
+      return i;
+  return 0;
+}
+
+//fills the list of endpoints.
+int fillEndpointList(char* cmd_str, int len, int* endpoint_list){
+  int ep = 0;
+  int i;
+  for(i = 0; i < len; i++){
+    if (cmd_str[i] == '?' || cmd_str[i] == '&'){
+      endpoint_list[ep] = i;
+      ep++;
+    }
+  }
+  endpoint_list[ep] = len;
+  return len;
+}
+
+void sendPage(WiFiClient c){
+  c.println("HTTP/1.1 200 OK");
+  c.println("Content-type:text/html");
+  c.println();
+
+
+  //print out html header
+
+  c.println("<html>"
+      "<head>"
+      "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
+      "<title>");
+
+  c.println(location) //location in the title
+
+    c.println("</title>"
+        "<style>"
+        "*{ font-family: sans-serif;"
+        " border-radius: 5px;"
+        "}"
+        "h1,h3{"
+        " text-align: center;"
+        "}"
+        "input[type=text]{"
+        " width: 100%;"
+        " padding: 5;"
+        " margin: 3;"
+        " margin-bottom: 25;"
+        " margin-top:10;"
+        " border: 1px solid darkgrey;"
+        "}"
+        "input[type=submit]{"
+        " background-color: #4286f4;"
+        " color: white;"
+        " padding: 10;"
+        " margin: 5%;"
+        " width: 90%;"
+        " font-weight: bold;"
+        " font-size: 24px;"
+        "}"
+        "</style>"
+        "</head>"
+        );
+
+  c.print("<h1>");
+  c.print(location);   //location in the header
+  c.println("</h1>");
+
+  c.println("<h3>Jaycar - Wireless Sign</h3>"
+      "<form>"
+      "<fieldset><legend>Dot Matrix Display</legend>"
+      "<input type='radio' name='op' option='text' checked>Show Text:<br>"
+      "<input type='text'><br>"
+      "<input type='radio' name='op' option='clock'>Show Clock<br>"
+      "<br>"
+      "<input type='submit' Value='Confirm'>"
+      "</fieldset>"
+      "</form>"
+      "</html>");
+  //final line to finish it off 
+  c.println();
+}
+
+
+
